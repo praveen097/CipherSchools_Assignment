7a3db8767ef338776b64575cd1231d204959e8d1
praveen097 <darlingpraveen097@gmail.com>
2021-02-14 08:31:57 +0530

    Assignment 1 solution 6

---
+++ Assign1Q6.cpp
@@ -0,0 +1,36 @@
+//Sort012
+#include<bits/stdc++.h>
+using namespace std;
+void sort(int arr[],int n){
+    int left = 0;
+    int right = n - 1;
+    int mid = 0;
+    while(mid <= right) {
+        if(arr[mid] == 0) {
+            int temp = arr[left];
+            arr[left] = arr[mid];
+            arr[mid] = temp;
+            left++;
+            mid++;
+        } else if(arr[mid] == 1) {
+            mid++;
+        } else {
+            int temp = arr[mid];
+            arr[mid] = arr[right];
+            arr[right] = temp;
+            right--;
+        }
+    }
+}
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    sort(arr,n);
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+}
