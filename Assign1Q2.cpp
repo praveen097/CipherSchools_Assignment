1db8c765fd553a1b93af9702cf0b9995415fa717
praveen097 <darlingpraveen097@gmail.com>
2021-02-14 09:00:59 +0530

    Assignment 1 solution 2

--- Assign1Q2.cpp
+++ Assign1Q2.cpp
@@ -18,7 +18,7 @@ int main(){
     for(int i=0;i<n;i++){
         cin>>arr[i];
     }
-    cout<<"Peasmallest index is/are: ";
+    cout<<"The smallest index is/are: ";
     if(n>2){
     for(int i=0;i<n;i++){
         if(arr[i]>arr[i-1] && arr[i]>arr[i+1]){
