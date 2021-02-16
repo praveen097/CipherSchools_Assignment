cdc6467b235687ab6ff0859a8fc711d3b86768a7
praveen097 <darlingpraveen097@gmail.com>
2021-02-16 16:37:34 +0530

    Assignment 2 solution 4

---
+++ Assignment2/Assign2Q4.java
@@ -0,0 +1,34 @@
+//Count Possible Decodings of a given Digit Sequence
+/**
+ * This questions is discussed in the class
+ */
+public class Assign2Q4 {
+
+    public static void printAlllDecoing(String number, String output) {
+
+        if(number.length() == 0) {
+            System.out.println(output);
+            return;
+        }
+        String firstCharacter = number.substring(0,1);
+        int firstNumber = Integer.parseInt(firstCharacter);
+        if(firstNumber != 0) {
+            char character = (char)('a' + firstNumber - 1);
+            printAlllDecoing(number.substring(1), output + character);
+        }
+        if(number.length() <= 1) {
+            return;
+        }
+        String firstTwoCharacters = number.substring(0,2);
+        int firstTwoNumber = Integer.parseInt(firstTwoCharacters);
+        if(firstTwoNumber <= 26) {
+            char character = (char)('a' + firstTwoNumber - 1);
+            printAlllDecoing(number.substring(2), output + character);
+        }
+    }
+public static void main(String[] args) {
+
+        printAlllDecoing("123", "");
+    }
+
+}
