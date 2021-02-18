22e24ff9a594b9898418ccedd59005ba2f84c793
praveen097 <darlingpraveen097@gmail.com>
2021-02-18 23:49:26 +0530

    Assignment 3 solution 9

---
+++ Assignment3/Assign3Q9.java
@@ -0,0 +1,90 @@
+import java.util.Scanner;
+
+public class Assign3Q9 {
+    public static Node mergeSort(Node head) {
+
+        if(head == null || head.next == null) {
+            return  head;
+        }
+
+        Node mid = getMid(head);
+
+        Node next = mid.next;
+        mid.next = null;
+
+
+        Node left = mergeSort(head);
+        Node right = mergeSort(next);
+
+        Node mergedLinkedListHead = merge(left, right);
+
+        return  mergedLinkedListHead;
+
+
+    }
+
+    private static Node merge(Node left, Node right) {
+
+        if(left == null) {
+            return right;
+        } else if (right == null) {
+            return left;
+        }
+
+        Node resultant = null;
+
+        if(left.data <= right.data) {
+            resultant = left;
+            resultant.next = merge(left.next, right);
+        } else {
+            resultant = right;
+            resultant.next = merge(left, right.next);
+        }
+
+        return resultant;
+    }
+
+    private static Node getMid(Node head) {
+
+        if(head == null) {
+            return head;
+        }
+
+        Node slow = head;
+        Node fast = head;
+
+        while(fast.next != null && fast.next.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+
+        return  slow;
+
+    }
+
+    public static void printList(Node head) {
+        Node current = head;
+
+        while(current != null) {
+            System.out.print(current.data + "  ->  ");
+            current = current.next;
+        }
+    }
+
+    public static void main(String[] args) {
+    
+        Node head = new Node(6);
+        head.next = new Node(3);
+        head.next.next = new Node(5);
+        head.next.next.next = new Node(1);
+
+
+        printList(head);
+
+        System.out.println();
+        Node result = mergeSort(head);
+
+        printList(result);
+
+    }
+}
